4b2313edd6b48f6117753c5596cfa1f3122d26bf
ArturoTrelles <trelles91n.n@gmail.com>
2025-12-31 16:38:18 -0600

    Bootstrap Spring Boot backend project
    Refs #13

---
+++ backend/src/test/java/com/team42/churninsight/ChurninsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team42.churninsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurninsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
