eb9f3dcda54b5824111b46b70c38d9d643a33cea
JsLealM <johanleal2002@gmail.com>
2026-01-04 17:30:18 -0500

    feat(back): agregar logica de calculo churn y metodo estatico

--- backend/src/main/java/com/team42/churninsight/domain/Prediction.java
+++ backend/src/main/java/com/team42/churninsight/domain/Prediction.java
@@ -14,8 +14,28 @@ import java.time.LocalDateTime;
 public class Prediction {
     private Long id;
     private String customerId;
+    private String transactionId;
     private Double probabilityChurn;
     private Churn churn;
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime createdAt;
+
+    public void calculateChurnStatus() {
+        if (this.probabilityChurn != null && this.probabilityChurn >= 0.5){
+            this.churn = Churn.CHURN;
+        }else {
+            this.churn = Churn.NO_CHURN;
+        }
+    }
+
+    public static Prediction create (String customerId, String transactionId, Double probabilityChurn){
+        Prediction newPrediction = new Prediction();
+        newPrediction.setCustomerId(customerId);
+        newPrediction.setTransactionId(transactionId);
+        newPrediction.setProbabilityChurn(probabilityChurn);
+        newPrediction.calculateChurnStatus();
+        newPrediction.setCreatedAt(LocalDateTime.now());
+        return newPrediction;
+    }
+
 }
 
