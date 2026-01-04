f8a11c2857422d6092fb07a8b3e459a38594016e
YoshuaPariona <loganpariona.1207@gmail.com>
2026-01-03 21:50:00 -0500

    feat(back): Añadir validaciones al dto PredictionResponse
    - Se añadieron validaciones mínimas de lo que se garantiza devolver en el dto.
    - Se cambió el tipo de probabilityChurn a BigDecimal para garantizar la exactitud.

--- backend/src/main/java/com/team42/churninsight/api/dto/PredictionResponse.java
+++ backend/src/main/java/com/team42/churninsight/api/dto/PredictionResponse.java
@@ -1,11 +1,23 @@
 package com.team42.churninsight.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
 
 public record PredictionResponse(
+        @NotNull
         @JsonProperty("customer_id")
         String customerId,
+
+        @NotNull
         Boolean churn,
+
+        @NotNull
+        @DecimalMin(value = "0.0")
+        @DecimalMax(value = "1.0")
         @JsonProperty("probability_churn")
-        Double probabilityChurn
+        BigDecimal probabilityChurn
 ) {}
