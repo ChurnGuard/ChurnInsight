8aa69c3c8a1d5f32e00c4cd987ec79749d534634
Damian Lambrecht <damianlambrecht@gmail.com>
2026-01-07 11:46:31 -0300

    fix/controller-backend
    se cambia @Controler por @RestController

--- backend/src/main/java/com/team42/churninsight/prediction/api/controller/PredictionController.java
+++ backend/src/main/java/com/team42/churninsight/prediction/api/controller/PredictionController.java
@@ -6,12 +6,12 @@ import com.team42.churninsight.prediction.api.dto.PredictionResponse;
 import com.team42.churninsight.prediction.service.PredictionServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/predictions")
 public class PredictionController {
 
