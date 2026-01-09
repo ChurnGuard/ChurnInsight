0ceae52a27a8020ad773e0cdfe4f3ed8800c62e2
Damian Lambrecht <damianlambrecht@gmail.com>
2026-01-09 14:47:38 -0300

    fix/fastapi-connection
    modifica fastapi para que la conexion a springboot funcione
    en el momento que se tenga el modelo de prediccion disponible se deberá configurar con todas las variables necesarias

--- ml-service/app/main.py
+++ ml-service/app/main.py
@@ -1,39 +1,22 @@
 from fastapi import FastAPI
-from app.schemas.prediction import PredictionRequest
+#from app.schemas.prediction import PredictionRequest
+from schemas.prediction import PredictionRequest
 import random
 
 app = FastAPI(title="ChurnInsight ML Service", version="1.0")
 
-
 @app.get("/")
 def health_check():
     return {"status": "ok", "service": "ml-service-v1"}
-
 
 @app.post("/predict", response_model=float)
 def predict_churn(data: PredictionRequest):
     """
     Endpoint temporal para simular la prediccion del modelo.
     """
+    print(data.age)
 
-    # LOGICA FALSA DE PREDICCION (se debe modificar por el modelo real, cuando el mismo este listo)
-    # simulamos que si hace mas de 60 dias no compra (recency > 60)
-    # y su frecuencia es baja, es probable que haga Churn.
+    probability = round(random.random(), 2)
+    print(probability)
 
-    risk_score = 0.0
-
-    if data.recency > 60:
-        risk_score += 0.4
-
-    if data.frequency < 3:
-        risk_score += 0.3
-
-    # si el cliente es un cazaofertas, el riesgo es alto si no hay ninguna ofertas
-    if data.discount_usage_rate > 0.5:
-        risk_score += 0.2
-
-    # Anadimos un poco de aleatoriedad para pruebas
-    noise = random.uniform(-0.1, 0.1)
-    final_prob = min(max(risk_score + noise, 0.0), 1.0)
-
-    return round(final_prob, 4)
+    return float(probability)
--- ml-service/app/schemas/prediction.py
+++ ml-service/app/schemas/prediction.py
@@ -2,38 +2,12 @@ from pydantic import BaseModel, Field
 
 
 class PredictionRequest(BaseModel):
-    # Datos demograficos
+
     age: int = Field(..., gt=0, description="Edad del cliente")
-    income_bracket: int = Field(..., description="Nivel de ingresos")
-
-    # Comportamiento de compra
-    recency: int = Field(..., description="Dias desde la ultima compra")
-    frequency: int = Field(..., description="Frecuencia de compra")
-    monetary: float = Field(..., description="Gasto total")
-    total_transactions: int = Field(
-        ..., description="Total de transacciones históricas"
-    )
-    avg_transaction_value: float = Field(
-        ..., description="Valor promedio por transacción"
-    )
-
-    # Variables calculadas
-    discount_usage_rate: float = Field(
-        ..., ge=0, le=1, description="% de uso de descuentos"
-    )
-    return_rate: float = Field(default=0.0, description="Tasa de devoluciones")
 
     class Config:
         json_schema_extra = {
             "example": {
-                "age": 34,
-                "income_bracket": 3,
-                "recency": 12,
-                "frequency": 5,
-                "monetary": 1250.50,
-                "total_transactions": 22,
-                "avg_transaction_value": 56.8,
-                "discount_usage_rate": 0.15,
-                "return_rate": 0.02,
+                "age": 34
             }
         }
